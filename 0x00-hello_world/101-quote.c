02106c498473a62d1ae0ceeb0c6e1f84fedf1a1f
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-03-16 07:10:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point.
+*
+* Return: 0 if success.
+*/
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
