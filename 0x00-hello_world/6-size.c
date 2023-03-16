8dbf5f590f639e1c45d41b82a8b1fe9c77d749e5
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-03-16 04:03:12 -0700

    6-size created

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* Main - Entry
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("size of a char: %d Byte(s)\n",sizeof(char));
+printf("size of an int: %d Byte(s)\n",sizeof(int));
+printf("size of a long int: %d Byte(s)\n",sizeof(long int));
+printf("size of a long long int: %d Byte(s)\n",sizeof(long long int));
+printf("size of a float: %d Byte(s)\n",sizeof(float));
+return (0);
+}
