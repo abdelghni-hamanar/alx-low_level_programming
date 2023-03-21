7fbe061ada2f17d042e006392c6243841340f21a
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-03-21 05:36:08 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - prints _putchar followed by new line
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
