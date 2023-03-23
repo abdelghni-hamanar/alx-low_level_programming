4eeb4fef576f78af95fbd6e437474016c73437c4
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-03-23 06:57:17 -0700

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_line - a fucntion that print line
+* @n: variable input
+* Return: void return nothing
+*/
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
+
