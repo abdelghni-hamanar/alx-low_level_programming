c7ed6a548db8c0c4c1b6fd352940523b3a45bd71
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-03-29 07:23:40 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
-*
-*
-*/
+ * *_strncat - a function that conctac the 1st string
+ * with number of n char of 2nd string
+ * @dest: pointer one variable
+ * @src: pointer 2nd variable
+ * @n: variable n character
+ * Return: Always dest when is Success
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int dlen = 0, i;
 
+	while (dest[dlen])
+		dlen++;
+
+	for (i = 0; i < n && src[i] != '0'; i++)
+		dest[dlen + i] = src[i];
+
+	dest[dlen + i] = '\0';
+
+	return (dest);
 }
