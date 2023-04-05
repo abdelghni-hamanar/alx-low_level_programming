2cc90f181c04ebb7398e39aefa38ca14809a7247
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-05 06:31:11 -0700

    0 puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _puts_recursion: a function that print string alphabet by
- *one using recursion
- * @s: pointer variable string
- * Return: void return nothing
- */
+* _puts_recursion - a function that set print string folloew by new line
+* @s: pointer variable
+* Return: void return nothing
+*/
+
 
 void _puts_recursion(char *s)
 {
