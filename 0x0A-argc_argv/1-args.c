1e0211e80a68e930a2d89a1ac6a654c00eb3ff3b
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-06 06:48:43 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* By Abdelghni Hamanar */
+/**
+* main - Entry point
+* @argc: variable
+* @argv: variable pointer
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
