4740203af7812a6b6df03f9f90d27725928cdf4b
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-06 07:48:17 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* By Abdelghni Hamanar */
+/**
+* main - Entry point
+* @argc: vriable
+* @argv: variable pointer
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
