d8efbedb29ca1fa463da7e9918917dcc9fea4f92
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-06 09:55:57 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* By Abdelghni Hamanar */
+/**
+* _islower- prints the alphabet in lowercase
+* @c: the variable
+* Return: void
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
