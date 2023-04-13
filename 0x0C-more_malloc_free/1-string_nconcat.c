18d25015d29d54c2f7e03266450fbb35ee152808
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-13 06:40:30 -0700

    str n concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/* By Abdelghni Hamanar */
+/**
+* *string_nconcat - Entry point
+* @s1: variable
+* @s2: variable
+* @n: variable
+* Return: pointer if Success, and NULL if Not
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
+	char *p;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	while (s1[len1])
+		len1++;
+
+	while (s2[len2])
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+
+	p = (char *) malloc((len1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < len1)
+	{
+		*(p + i) = *(s1 + i);
+		i++;
+	}
+
+	while (i < (len1 + n))
+	{
+		*(p + i) = *(s2 + j);
+		i++;
+		j++;
+	}
+	*(p + i) = '\0';
+
+	return (p);
+}
