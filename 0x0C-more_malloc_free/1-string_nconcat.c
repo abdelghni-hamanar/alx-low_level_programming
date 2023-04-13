edd0b2c3ac87544fdaf82a97cb089a57e298fcb1
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-13 06:44:35 -0700

    strnconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,13 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-
-	while (s1[len1])
+	while (s1 && s1[len1])
 		len1++;
 
-	while (s2[len2])
+	while (s2 && s2[len2])
 		len2++;
 
 	if (n >= len2)
