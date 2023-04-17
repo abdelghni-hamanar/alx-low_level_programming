1af1d832bc5cf01780b020f62e91c1fd581aba83
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-17 07:50:16 -0700

    dog h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - a dog into
+* @name: dog name
+* @age: dog age
+* @owner: owner dog name
+*
+* Description: best description
+*/
+
 struct dog {
 
 	char *name;
 	float age;
 	char *owner;
 };
+
+
 
 
 
