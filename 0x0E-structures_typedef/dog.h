e0179eadde36b017475f59e2dc186a06aeb33971
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-17 08:26:17 -0700

    dog h 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,12 @@ struct dog
 	char *owner;
 };
 
+/**
+* dog_t - typedef for struct
+*/
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
