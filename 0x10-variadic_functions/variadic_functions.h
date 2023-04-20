c0a9b6b3b3d7db1c6ed710f3f6641f6f8891d05f
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-20 14:27:19 -0700

    variadic summ it all

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VAR_FUNCTIONS_H
+#define VAR_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int sum_them_all(const unsigned int n, ...);
+
+
+
+
+
+#endif
