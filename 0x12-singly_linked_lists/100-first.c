32905136a040aa848fc67ac9b7f67fd080558c7a
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-27 04:31:05 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/* By Abdelghni Hamanar */
+/**
+ * the_one - Entry point
+ * Return: void
+ */
+
+void the_one(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n ");
+}
