f71fc53d09cc8ee0e06be0d7a507a086552ab63d
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-27 04:44:18 -0700

    1st

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-
 void first(void) __attribute__ ((constructor));
 
 /* By Abdelghni Hamanar */
 /**
- * first - Entry point
- * Return: void
+ * first - prints message befor main
+ * function executed
  */
 
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n ");
+	printf("I bore my house upon my back!\n");
 }
